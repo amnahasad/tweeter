b0a7c442b2552e32adffe2f6b9bab5c37e170536
Amnah Asad <amnah_asad@hotmail.com>
2021-11-17 01:07:50 +0000

    Implemented the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+//this file is responsible for the character counter
+
+$(document).ready(function() {
+    $('#tweet-text').on('input', function() {
+        console.log(this);
+        let inputLength = $(this).val().length;
+        let counter = $('.counter');
+        counter.val(140 - inputLength);
+
+        if (counter.val() < 0) {
+            counter.css('color', 'red');
+        }
+    });
+  });
+        
